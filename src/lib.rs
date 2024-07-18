22df4fcc7261c06fc05192e4041d6e47d71ff869
Adam Rizkalla <adam@saronic.com>
2024-07-18 00:42:01 -0500

    Add `once_cell` as a crate dependency
    Adding `once_cell` as a crate dependency and using the `$crate` path
    scoping prefix removes the need to add `once_cell` as a dependency to
    the calling crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+pub extern crate once_cell;
+
 #[macro_export]
 macro_rules! defvar {
     { $name:ident: $type:ty = $default:expr, or try $var:ident => $transform:block $(;)? } => {
-        static $name: ::once_cell::sync::Lazy<$type> = ::once_cell::sync::Lazy::new(|| {
+        static $name: $crate::once_cell::sync::Lazy<$type> = $crate::once_cell::sync::Lazy::new(|| {
             match ::std::env::var(stringify!($name)) {
                 Ok($var) => {
                     match $transform {
